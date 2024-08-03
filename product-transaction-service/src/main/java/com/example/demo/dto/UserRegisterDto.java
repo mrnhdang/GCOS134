b6e6ab47c10049a4c012d4b47e1ec4980fd1ba29
thien <thienlaprocao1@gmail.com>
2024-08-04 02:46:39 +0700

    Week 2: Update User table complete

--- product-transaction-service/src/main/java/com/example/demo/dto/UserRegisterDto.java
+++ product-transaction-service/src/main/java/com/example/demo/dto/UserRegisterDto.java
@@ -2,13 +2,19 @@ package com.example.demo.dto;
 
 public class UserRegisterDto {
     private String username;
+    private String address;
+    private String phonenumber;
     private String password;
     private String email;
+    private String role;
 
-    public UserRegisterDto(String username, String password, String email) {
+    public UserRegisterDto(String username, String address, String phonenumber, String password, String email, String role) {
         this.username = username;
+        this.address = address;
+        this.phonenumber = phonenumber;
         this.password = password;
         this.email = email;
+        this.role = role;
     }
 
     public String getUsername() {
@@ -17,6 +23,22 @@ public class UserRegisterDto {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
     }
 
     public String getPassword() {
@@ -33,6 +55,14 @@ public class UserRegisterDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
 
--- product-transaction-service/src/main/java/com/example/demo/repository/UserRepository.java
+++ product-transaction-service/src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
     public Optional<User> findByUsername(String username);
+
     public Optional<User> findByUsernameAndPassword(String username, String password);
 }
